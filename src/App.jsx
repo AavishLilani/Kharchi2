4eada93ba1987180af5a96bc53aba8ee8abc0e50
unknown <aavishlilani090304@gmail.com>
2025-02-13 12:01:31 +0530

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import { Route, Routes } from 'react-router-dom'
-import Loginpage from './pages/Loginpage'
-import SignupPage from './pages/SignupPage'
-import { Landing } from './pages/Landing'
-
-
+import { Route, Routes } from 'react-router-dom';
+import Loginpage from './pages/Loginpage';
+import SignupPage from './pages/SignupPage';
+import { Landing } from './pages/Landing';
 
 function App() {
-  console.log("inside app")
+  console.log("inside app");
+
   return (
-
-
     <Routes>
       <Route path='/' element={<Landing />} />
       <Route path='/signin' element={<Loginpage />} />
       <Route path='/signup' element={<SignupPage />} />
     </Routes>
-  )
-
-
+  );
 }
 
-export default App
+export default App;
