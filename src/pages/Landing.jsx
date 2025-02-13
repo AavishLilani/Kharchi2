6881e734b128ac2d6ac1488ce6ff9330b3e1c87f
unknown <aavishlilani090304@gmail.com>
2025-02-13 12:06:57 +0530

    test1

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Landing=()=>{
 console.log("Landing");
-
+return(
+    <div>
+        
+        <Link className="text-white bg-black" to={"/signin"}/>
+        <Link className="text-white bg-black" to={"/signup"}/>
+    </div>
+)
 }
